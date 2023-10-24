d0b27d965ecf426fa59e26390531f51a18ac5e4b
Philipp Dormann <philipp@philippdormann.de>
2023-10-24 13:45:54 +0200

    fix: sevdesk api speed timeout

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import axios, { AxiosHeaderValue } from "axios";
 export function sevDeskLogin(sevDeskApiKey: AxiosHeaderValue) {
   const instance = axios.create({
     baseURL: "https://my.sevdesk.de/api/v1",
-    timeout: 5000,
+    timeout: 25000,
     headers: { Authorization: sevDeskApiKey },
   });
   serviceOptions.axios = instance;
